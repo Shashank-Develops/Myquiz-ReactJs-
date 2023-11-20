f34e78f79516e759db331e58ecbb87c3c428700b
Shashankdhauni <112565970+Shashankdhauni@users.noreply.github.com>
2023-11-20 12:39:04 +0530

    Updates

---
+++ precache-manifest.b353321eb048eef786eb61ece6adacc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e5d8034ee7c39cf704ac9ef2173c04d",
+    "url": "/Myquiz-ReactJs-/index.html"
+  },
+  {
+    "revision": "5064125858542a13dcd1",
+    "url": "/Myquiz-ReactJs-/static/css/main.26b1dd8e.chunk.css"
+  },
+  {
+    "revision": "deacecf959496c1df5e7",
+    "url": "/Myquiz-ReactJs-/static/js/2.45a5db28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Myquiz-ReactJs-/static/js/2.45a5db28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5064125858542a13dcd1",
+    "url": "/Myquiz-ReactJs-/static/js/main.e6ad88bd.chunk.js"
+  },
+  {
+    "revision": "90da62d37b53618e0058",
+    "url": "/Myquiz-ReactJs-/static/js/runtime-main.3490e839.js"
+  }
+]);
